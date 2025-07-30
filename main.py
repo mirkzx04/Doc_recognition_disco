8668cb40b2cb7956b3493dac939d7973c598d9da
Mirko Bonifazi <mirkzx@Mac.station>
2025-07-30 22:56:30 +0200

    Adding load dataset function

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from disco-project!")
+import os
+import cv2
+import numpy as np
 
+def load_dataset(dataset_path = './dataset_pdf_v1/images'):
+    """
+    Loading document image
+    """
+    imgs = []
+    tst = 0
+
+    for filename in os.listdir(dataset_path):
+        # Check file extension
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+
+            # Read image with Open-CV
+            img_path = os.path.join(dataset_path, filename)
+            img = cv2.imread(img_path)
+
+            if img is not None:
+                imgs.append(imgs)
+        
+        tst += 1
+
+        if tst == 5:
+            break
+
+    return np.array(imgs)
 
 if __name__ == "__main__":
-    main()
+    image = load_dataset()
+    
+    
+
